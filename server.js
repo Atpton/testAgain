85a122e55cec000f14ebf6f70d24ff5c111f3c9e
Tonstory <auttapon.siriwaramas@gmail.com>
2020-07-05 01:18:44 +0700

    Update

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const express = require('express');
-var app = express();
-var port =process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 80;
+const path = require('path');
 app.get("/",(req,res)=>{
     res.send("Hello word");
 });
+
+app.get("/dashboard",(req,res,next)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+});
+
 http.createServer(app).listen(port,()=>{
     console.log("Create Server port : "+port);
 });
